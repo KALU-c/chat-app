7b5baf9339003f2825e867241a0021b940d84dd2
Endekalu-Zemenu <endekaluzemenu2134@gmail.com>
2024-08-27 08:37:00 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from "socket.io";
+
+const app = express();
+const port = 3000;
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  })
+});
+
+server.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
